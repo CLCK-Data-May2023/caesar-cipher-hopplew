35b38dee61a1dcdf214ce7a9fc83e024e374b8a8
Will Anderson <hopplew@gmail.com>
2023-07-26 19:31:18 -0400

    update

--- cipher.py
+++ cipher.py
@@ -36,5 +36,5 @@ for c in text:
         c = cipher_dict[c]
     cipher_text += c
 
-print("Please enter a sentence:", text)
-print("The encrypted sentence is:", cipher_text)
+print("please enter a sentence:", text)
+print("the encrypted sentence is:", cipher_text)
